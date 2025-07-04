0544ef2fd35799ae20649aa1dc5ad5e75efa0bd6
pasindunimesha <r.m.p.n.rathnayake@gmail.com>
2025-07-04 02:35:15 -0400

    finished task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -53,7 +53,18 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const booksByTitle = [];
+  for (let key in books) {
+    if (books[key].title.toLowerCase() === title.toLocaleLowerCase()){
+        booksByTitle.push({isbn: key, ...books[key]});
+    }
+  }
+  if (booksByTitle.length > 0){
+    res.status(200).send(JSON.stringify(booksByTitle, null, 4));
+  } else {
+    res.status(404).json({message: "No books found"});
+  }
 });
 
 //  Get book review
